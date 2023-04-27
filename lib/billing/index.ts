83814345e54ffede67198f1b03fc0b1d90cfa25c
Alex Harvey <alex.harvey@shopify.com>
2023-04-27 10:40:55 -0400

    added unsubscribe function for the billing service

--- lib/billing/index.ts
+++ lib/billing/index.ts
@@ -4,22 +4,6 @@ import {check} from './check';
 import {request} from './request';
 import {cancel} from './cancel';
 
-/**
- * TODO:
- * I need to add the ability to cancel or downgrade a user from a paid plan
- *  using the appSubscriptionCancel mutation in the graphql api
- *  https://shopify.dev/docs/api/admin-graphql/2023-01/mutations/appSubscriptionCancel
- *
- * it will be a curried function like request and check
- *
- * I will need:
- * config: ConfigInterface
- *
- * Mutation: appSubscriptionCancel
- * Input: id: ID! for the app subscription id
- * Input: returnUrl: String! for the url to redirect the user to after the cancellation
- *
- *  */
 export function shopifyBilling(config: ConfigInterface) {
   return {
     check: check(config),
